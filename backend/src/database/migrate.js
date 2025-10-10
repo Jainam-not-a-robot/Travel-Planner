41cf3a155e8e4ea5a523b66057b1aca5677934de
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2025-10-10 15:38:50 +0530

    Added backup db

--- backend/src/database/migrate.js
+++ backend/src/database/migrate.js
@@ -1,9 +1,9 @@
-import pool from './db.js';
+import pool from './pool.js';
 import fs from 'fs';
 
 async function runMigration() {
   try {
-    const sql = fs.readFileSync('mydb.sql', 'utf8');
+    const sql = fs.readFileSync('pool.sql', 'utf8');
     await pool.query(sql);
     console.log('Migration completed!');
   } catch (err) {
