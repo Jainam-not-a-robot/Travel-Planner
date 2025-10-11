3af3bf75514f53034b825c7cfe4f5505c10d07bf
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2025-10-11 10:56:10 +0530

    Removed consoling backend url

--- frontend/src/app/places/places.tsx
+++ frontend/src/app/places/places.tsx
@@ -28,7 +28,6 @@ export default function Places() {
     const { selectedPlace, selectedOption } = context;
     const backendURL=process.env.NEXT_PUBLIC_BACKEND_URL;
     useEffect(() => {
-        console.log(backendURL)
         axios.get<{ places: Place[] }>(`${backendURL}/api/places/states/${selectedPlace}`)
             .then(response => setPlaces(response.data.places))
             .catch(error => console.error(error));
