73bf82ddb1b0c437095fbf7719ecf14f44a0b8a6
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2025-10-13 22:39:57 +0530

    fixed map size issue

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,21 @@
 import { IndiaMap } from "./india-map"
 import Search from "./search-option1"
 import NavCard from "./NavCard"
+import '@/app/india-map.css'
 export default function Home() {
   return (
     <div className="w-full relative">
       {/* <SidebarTrigger className="mt-32 fixed"/> */}
       <NavCard></NavCard>
-      <Search/>
-    <div className="flex justify-center items-center">
-      <IndiaMap/>
-    </div>
-    
+      <Search />
+      <div className="flex justify-center items-center">
+        <div className="div-container">
+          <div className="india-map-container">
+            <IndiaMap />
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
