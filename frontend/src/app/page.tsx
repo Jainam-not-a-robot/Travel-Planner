1f0909ccf109434ff2b5bcefed32d8489149e41b
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2025-10-13 22:50:59 +0530

    connecting backend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,18 @@
+'use client'
 import { IndiaMap } from "./india-map"
 import Search from "./search-option1"
 import NavCard from "./NavCard"
 import '@/app/india-map.css'
+import { useEffect } from "react"
+import axios from "axios"
 export default function Home() {
+  
+    useEffect(()=> {
+      const backendURL=process.env.NEXT_PUBLIC_BACKEND_URL;
+        axios.get(`${backendURL}`)
+            .then(()=>console.log("backend connecting"))
+            .catch((err)=>console.log(err));
+    }, []);
   return (
     <div className="w-full relative">
       {/* <SidebarTrigger className="mt-32 fixed"/> */}
