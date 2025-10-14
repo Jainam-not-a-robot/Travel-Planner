0ea999d718230f5e43dcfbfa40a91039c2438798
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2025-10-14 18:43:40 +0530

    Improved loading spinner

--- frontend/src/app/places/loading_page.tsx
+++ frontend/src/app/places/loading_page.tsx
@@ -1,5 +1,9 @@
-export default function Loading(){
-    return(
-        <h2>Loading...</h2>
+import { Spinner } from "@/components/ui/spinner"
+export default function Loading() {
+    return (
+        <div className="flex items-center justify-center min-h-9/12">
+            <Spinner />
+            <div>Loading...</div>
+        </div>
     )
 }
